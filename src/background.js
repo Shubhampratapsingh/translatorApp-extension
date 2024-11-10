00e89c1328b2655aae32b6d6bc05dd18db99ca18
Shubham Pratap Singh <singh.gaya101@gmail.com>
2024-11-10 17:35:53 +0530

    popup fix

--- src/background.js
+++ src/background.js
@@ -1,3 +1,13 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Hello World Extension Installed");
+  console.log("Extension Installed");
+});
+chrome.action.onClicked.addListener(() => {
+  chrome.windows.create({
+    url: chrome.runtime.getURL("index.html"),
+    type: "popup",
+    width: 500,
+    height: 500,
+    top: 100,
+    left: 100,
+  });
 });
