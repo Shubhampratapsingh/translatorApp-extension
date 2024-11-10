b1e7b4eb138c9de337a0d29dc5a075d9895a44b1
Shubham Pratap Singh <singh.gaya101@gmail.com>
2024-11-10 19:21:04 +0530

    deployed server url

--- src/constants/api-constants.js
+++ src/constants/api-constants.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:9000";
+// export const API_URL = "http://localhost:9000";
+export const API_URL = "https://translatorapp-server.onrender.com";
 export const API_END_POINTS = {
   ADD_TRANSCRIPT: `${API_URL}/addTranscript`,
   GET_TRANSCRIPT: `${API_URL}/getTranscript`,
